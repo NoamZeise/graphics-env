bd1004c7c4b0d38e1bc59137b0addb06291b2f1c
NoamZeise <noam4134@gmail.com>
2023-03-21 19:13:39 +0000

    extracted framework cli option to seperate function

--- src/main.cpp
+++ src/main.cpp
@@ -7,47 +7,54 @@
 #include <cstring>
 
 
-int main(int argc, char** argv)
-{
-    try
-	{
-#ifndef NDEBUG
-		std::cout << "In debug mode" << std::endl;
-#endif
-		RenderFramework defaultFramework = RenderFramework::VULKAN;
-		for(int i = 1; i < argc; i++) {
-		    if(strcmp(argv[i], "-r") == 0) {
-			if(i + 1 < argc) {
-			    i++;
-			    if(strcmp(argv[i], "opengl") == 0) {
-				defaultFramework = RenderFramework::OPENGL;
-				std::cout << "default framework opengl selected\n";
-			    }
-			    if(strcmp(argv[i], "vulkan") == 0) {
-				defaultFramework = RenderFramework::VULKAN;
-				std::cout << "default framework vulkan selected\n";
-			    }
-			}
-		    }
-		}
-		App app(defaultFramework);
-		app.run();
-	}
-	catch (const std::exception& e)
-	{
-		#ifndef NDEBUG
-		std::cerr << e.what() << std::endl;
-		#else
-		std::ofstream crashFile("CrashInfo.txt");
-		if (crashFile.is_open())
-		{
-			crashFile.seekp(0);
-			crashFile << e.what();
-			crashFile.close();
-		}
-		#endif
-		return EXIT_FAILURE;
-	}
+void frameworkArg(int argc, char** argv, int i, RenderFramework *framework);
 
-	return EXIT_SUCCESS;
+
+int main(int argc, char** argv) {
+  try
+    {
+#ifndef NDEBUG
+      std::cout << "In debug mode" << std::endl;
+#endif
+      RenderFramework framework = RenderFramework::VULKAN;
+      for(int i = 1; i < argc; i++) {
+	frameworkArg(argc, argv, i, &framework);
+      }
+      App app(framework);
+      app.run();
+    }
+  catch (const std::exception& e)
+    {
+#ifndef NDEBUG
+      std::cerr << e.what() << std::endl;
+#else
+      std::ofstream crashFile("CrashInfo.txt");
+      if (crashFile.is_open())
+	{
+	  crashFile.seekp(0);
+	  crashFile << e.what();
+	  crashFile.close();
+	}
+#endif
+      return EXIT_FAILURE;
+    }
+
+  return EXIT_SUCCESS;
+}
+
+
+void frameworkArg(int argc, char** argv, int i, RenderFramework *framework) {
+  if(strcmp(argv[i], "-r") == 0) {
+    if(i + 1 < argc) {
+      i++;
+      if(strcmp(argv[i], "opengl") == 0) {
+	*framework = RenderFramework::OPENGL;
+	std::cout << "default framework opengl selected\n";
+      }
+      if(strcmp(argv[i], "vulkan") == 0) {
+	*framework = RenderFramework::VULKAN;
+	std::cout << "default framework vulkan selected\n";
+      }
+    }
+  }
 }
