431cc985246d3e7493e19267d21084bb445df377
Noam Zeise <noam4134@gmail.com>
2023-12-11 19:36:52 +0000

    fixed negative time on first update on some platforms

--- libs/GameHelper/src/timer.cpp
+++ libs/GameHelper/src/timer.cpp
@@ -13,6 +13,8 @@ void Timer::update() {
     currentUpdate = hrc::now();
     elapsed = std::chrono::duration_cast<std::chrono::milliseconds>
 	(currentUpdate - lastUpdate).count();
+    if(elapsed < 0) //first update can be negative
+	elapsed = 0;
 }
 
 long long Timer::dt() {
