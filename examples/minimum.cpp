90ab5f342b76aa8ba12ed0842d72d12952c74e9d
Noam Zeise <noam4134@gmail.com>
2024-03-29 02:23:32 +0000

    added comments to minimum example

--- examples/minimum.cpp
+++ examples/minimum.cpp
@@ -1,7 +1,7 @@
 #include <graphics/manager.h>
-#include <game/camera.h>
 #include <graphics/glm_helper.h>
-#include <glm/gtc/matrix_inverse.hpp>
+// for parseArgs - choose opengl or vulkan backend
+#include "helper.h"
 
 // This example shows minimum functionality of the graphics environment
 // With none of the optional libs
@@ -9,36 +9,35 @@
 // The program draws a textured quad
 // Press Esc to exit.
 
-#include "helper.h"
-
 int main(int argc, char** argv) {
+    // setup renderer with desired state
     ManagerState state;
     state.windowTitle = "minimum example";
-    state.defaultRenderer = parseArgs(argc, argv, &state.windowTitle);
-
-    state.render.forceFinalBuffer = true;
-    
+    state.defaultRenderer = parseArgs(argc, argv, &state.windowTitle);    
     Manager manager(state);
     
     ResourcePool* pool = manager.render->pool();
+    // load resources to cpu
     Resource::Texture tex = pool->tex()->load("textures/tile.png");
-
+    // load resources to gpu
     manager.render->LoadResourcesToGPU(pool);
     manager.render->UseLoadedResources();
 
     while(!glfwWindowShouldClose(manager.window)) {
+	// update timers, input, etc
 	manager.update();
-	
+
+	// Press Esc to exit
 	if(manager.input.kb.press(GLFW_KEY_ESCAPE))
 	    glfwSetWindowShouldClose(manager.window, GLFW_TRUE);
 	
 	if(manager.winActive()) {
-	    glm::vec2 mousePos = manager.mousePos();
-	    
+	    // draw texture where mouse is
             manager.render->DrawQuad(
 		    tex,
 		    glmhelper::calcMatFromRect(
-			    glm::vec4(mousePos.x, mousePos.y, 200.0f, 200.0f), 0.0f, 0.5f),
+			    glm::vec4(manager.mousePos().x, manager.mousePos().y, 200.0f, 200.0f)
+			    , 0.0f, 0.5f),
 		    glm::vec4(1.0f),
 		    glmhelper::getTextureOffset(
 			    tex.dim, glm::vec4(0.0f, 0.0f, tex.dim.x * 5, tex.dim.y * 5)));
